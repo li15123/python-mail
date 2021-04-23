1be4a144b4470e7ac9a3915635d56aad717d1bae
malcolm <lizhenjun_1270@163.com>
2021-04-23 11:01:33 +0800

    添加新文件

---
+++ main.py
@@ -0,0 +1,30 @@
+#coding=UTF-8
+import json
+from email163 import loginEmail163
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def get_emailType(myEmail):
+    myEmail = myEmail.split(".")
+    myEmail = myEmail[0].split("@")
+    return myEmail[1]
+
+
+#load serverName
+with open('./serverNameConfig.json','r')as severJson:
+    server_json = json.load(severJson)
+
+#load email
+with open('./email.json','r')as emailJson:
+    email_json = json.load(emailJson)    
+
+#print('This is a \033[1;35m %s \033[0m!' %user)
+
+for user in email_json:
+    for password in email_json[user]:
+        emailType = get_emailType(user)
+        if emailType == "163":
+            if loginEmail163(user, password) :
+                break
